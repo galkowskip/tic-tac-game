785c595df27b1ca94523acbe2dd9400d798aff97
pawel.g <galkowski.p97@gmail.com>
2023-06-17 17:33:19 +0200

    firebase hosting connected

--- src/components/GameBoard.test.tsx
+++ src/components/GameBoard.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import GameBoard from "./GameBoard.tsx";
+import GameBoard from "./GameBoard.js";
 
 let gameState = [[], [], []];
 let setGameState = (value) => {
