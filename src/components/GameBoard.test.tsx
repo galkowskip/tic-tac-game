53845b31b0be67c590ec260baf10ecbc15ef0fd4
pawel.g <galkowski.p97@gmail.com>
2023-06-17 17:35:52 +0200

    game board fix

--- src/components/GameBoard.test.tsx
+++ src/components/GameBoard.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import GameBoard from "./GameBoard.js";
+import GameBoard from "./GameBoard.tsx";
 
 let gameState = [[], [], []];
 let setGameState = (value) => {
