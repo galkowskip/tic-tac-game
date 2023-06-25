adb5010bd2440483ec83233ae13f04761da5b4f9
pawel.g <pawel.g@rsstudio.net>
2023-06-25 17:20:19 +0200

    if not logged go to /login

--- src/components/GameWidget.tsx
+++ src/components/GameWidget.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 // import LineCanvas from "./LineCanvas.tsx";
 import GameBoard from "./GameBoard.tsx";
+import { AuthContext, PlayerContext } from "../contexts.ts";
+
+import { Navigate } from "react-router-dom";
 
 import "./GameWidget.scss";
 
@@ -10,10 +13,13 @@ function GameWidget() {
     [false, false, false],
     [false, false, false],
   ]);
+
   const [currentPlayer, setCurrentPlayer] = React.useState<string>("X");
   const [isWinner, setIsWinner] = React.useState<Boolean>(false);
   const [isDraw, setIsDraw] = React.useState<Boolean>(false);
 
+  const user = useContext(AuthContext);
+  const player = useContext(PlayerContext);
 
   function toggleCurrentPlayer(): void {
     if (currentPlayer === "X") {
@@ -33,22 +39,26 @@ function GameWidget() {
     }
   }
 
-  return (
-    <div className="game-widget">
-      <h1>Game Widget</h1>
-      <p className="game-state">{currentGameState()}</p>
-      <GameBoard
-        gameState={gameState}
-        setGameState={setGameState}
-        isWinner={isWinner}
-        setIsWinner={setIsWinner}
-        isDraw={isDraw}
-        setIsDraw={setIsDraw}
-        currentPlayer={currentPlayer}
-        toggleCurrentPlayer={toggleCurrentPlayer}
-      />
-    </div>
-  );
+  if (!user || !player) {
+    return <Navigate to="/login" />;
+  } else {
+    return (
+      <div className="game-widget">
+        <h1>Game Widget</h1>
+        <p className="game-state">{currentGameState()}</p>
+        <GameBoard
+          gameState={gameState}
+          setGameState={setGameState}
+          isWinner={isWinner}
+          setIsWinner={setIsWinner}
+          isDraw={isDraw}
+          setIsDraw={setIsDraw}
+          currentPlayer={currentPlayer}
+          toggleCurrentPlayer={toggleCurrentPlayer}
+        />
+      </div>
+    );
+  }
 }
 
 export default GameWidget;
